672f93e23b7b71c5ef9c3614e54b4f35250452db
nancygupta6 <67143528+nancygupta6@users.noreply.github.com>
2020-07-15 11:54:37 +0530

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -13,13 +13,14 @@
 // See Lecture 52, part 2
 
 (function (window) {
-var helloSpeaker ={};
- 
+
+var helloSpeaker ={}; 
 var speakWord = "Hello";
+
 helloSpeaker.speak = function (name){
   console.log(speakWord + " " + name);  
 }
-window.helloSpeaker = helloSpeaker;
+window.helloSpeaker=helloSpeaker;
   
 })(window);
 
